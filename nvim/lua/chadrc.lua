d349f7e7c7795ddb73f0784f4fbe6e0a586724ec
Keith Henderson <keith.donaldh@gmail.com>
2025-05-20 15:11:19 -0400

    update nvim

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,17 +1,23 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "gruvbox",
+  theme = "gruvbox",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
+}
+
+M.ui = {
+  telescope = {
+    style = "bordered",
+  },
 }
 
 -- M.nvdash = { load_on_startup = true }
@@ -21,16 +27,16 @@ M.base46 = {
 --      }
 --}
 M.nvdash = {
-    load_on_startup = true,
+  load_on_startup = true,
 
-    header = {
-        "██████╗ ██╗███████╗███████╗   ██╗   ██╗██╗███╗   ███╗",
-        "██╔══██╗██║╚══███╔╝╚══███╔╝   ██║   ██║██║████╗ ████║",
-        "██████╔╝██║  ███╔╝   ███╔╝    ██║   ██║██║██╔████╔██║",
-        "██╔══██╗██║ ███╔╝   ███╔╝     ╚██╗ ██╔╝██║██║╚██╔╝██║",
-        "██║  ██║██║███████╗███████╗██╗ ╚████╔╝ ██║██║ ╚═╝ ██║",
-        "╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
-    },
+  header = {
+    "██████╗ ██╗███████╗███████╗   ██╗   ██╗██╗███╗   ███╗",
+    "██╔══██╗██║╚══███╔╝╚══███╔╝   ██║   ██║██║████╗ ████║",
+    "██████╔╝██║  ███╔╝   ███╔╝    ██║   ██║██║██╔████╔██║",
+    "██╔══██╗██║ ███╔╝   ███╔╝     ╚██╗ ██╔╝██║██║╚██╔╝██║",
+    "██║  ██║██║███████╗███████╗██╗ ╚████╔╝ ██║██║ ╚═╝ ██║",
+    "╚═╝  ╚═╝╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+  },
 }
 
 return M
--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,4 +3,6 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "pylsp" }
 vim.lsp.enable(servers)
 
+vim.o.winborder = "single"
+
 -- read :h vim.lsp.config for changing options of lsp servers
