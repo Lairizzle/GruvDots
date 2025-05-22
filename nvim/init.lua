fa797ea62d00526c4927319ffd046daf02e640b1
Keith Henderson <keith.donaldh@gmail.com>
2025-05-22 10:28:06 -0400

    update configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,6 @@
 require("config.lazy")
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
+
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
