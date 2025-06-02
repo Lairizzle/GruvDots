a73a15898db8066170eac3665554562960600d05
Keith Henderson <keith.donaldh@gmail.com>
2025-06-02 08:30:17 -0400

    update

--- nvim/lua/custom/plugins/noice.lua
+++ nvim/lua/custom/plugins/noice.lua
@@ -36,6 +36,5 @@ return {
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
-    opts = { background_colour = '#000000' },
   },
 }
